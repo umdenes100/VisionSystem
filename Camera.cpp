4beeb378abb736bc41d0a249353debc29be355f2
Mukul Ashwath Ram <mukul.ram97@gmail.com>
2018-10-31 11:28:12 -0400

    Marker: 3x3 => 4x4

--- Camera.cpp
+++ Camera.cpp
@@ -20,7 +20,7 @@ void Camera::capture() {
 
         std::vector<int> markerIds;
         std::vector<std::vector<cv::Point2f>> markerCorners;
-        cv::Ptr<cv::aruco::Dictionary> markerDictionary = cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME::DICT_ARUCO_ORIGINAL);
+        cv::Ptr<cv::aruco::Dictionary> markerDictionary = cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
 
         cv::aruco::detectMarkers(image, markerDictionary, markerCorners, markerIds);
         cv::aruco::drawDetectedMarkers(image, markerCorners, markerIds);
